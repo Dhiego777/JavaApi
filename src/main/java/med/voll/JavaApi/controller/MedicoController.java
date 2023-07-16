b97bbbb6660ac0c6429c01d6e66b0c4462c24d35
Dhiego Cavalcante <dhiego-cavalcante@hotmail.com>
2023-07-16 10:37:15 -0300

    Adicao do endpoint get e criacao do dto para retornar apenas 4 itens do jpa

--- src/main/java/med/voll/JavaApi/controller/MedicoController.java
+++ src/main/java/med/voll/JavaApi/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.JavaApi.controller;
 
 import jakarta.validation.Valid;
 import med.voll.JavaApi.medico.CadastroMedico;
+import med.voll.JavaApi.medico.DadosListagemMedico;
 import med.voll.JavaApi.medico.Medico;
 import med.voll.JavaApi.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,7 +21,11 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody  @Valid CadastroMedico cadastroMedico){
-//        System.out.println(cadastroMedico);
         repository.save(new Medico(cadastroMedico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
